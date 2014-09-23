548384278b024cb701b58a0c950f426f38de2e64
hiiamkyj <hiiamkyj@gmail.com>
2014-09-23 15:54:50 +0900

    blogmapper

--- src/main/java/mybatis/MyBatisGSTest.java
+++ src/main/java/mybatis/MyBatisGSTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -12,7 +13,16 @@ public class MyBatisGSTest {
 	public static void main(String[] args) throws IOException {
 		String resource = "org/mybatis/example/mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(inputStream);
+
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+		  BlogMapper mapper = session.getMapper(BlogMapper.class);
+		  Blog blog = mapper.selectBlog(101);
+		} finally {
+		  session.close();
+		}
 	}
 
 }
