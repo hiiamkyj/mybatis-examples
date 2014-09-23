fe67037fb23ab9fa2b7caa96b4d2dfadc25f284b
hiiamkyj <hiiamkyj@gmail.com>
2014-09-23 17:48:52 +0900

    blog attribute

--- src/main/java/mybatis/MyBatisGSTest.java
+++ src/main/java/mybatis/MyBatisGSTest.java
@@ -10,18 +10,23 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.example.Blog;
 import org.mybatis.example.BlogMapper;
 
+/**
+ * http://mybatis.github.io/mybatis-3/ko/getting-started.html
+ * @author kenu
+ *
+ */
 public class MyBatisGSTest {
 
 	public static void main(String[] args) throws IOException {
 		String resource = "org/mybatis/example/mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
-				.build(inputStream);
-
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
 		SqlSession session = sqlSessionFactory.openSession();
 		try {
 		  BlogMapper mapper = session.getMapper(BlogMapper.class);
-		  Blog blog = mapper.selectBlog(101);
+		  Blog blog = mapper.selectBlog(1);
+		  System.out.println(blog);
 		} finally {
 		  session.close();
 		}
